77369e7d6e3434d7dd2e478bd661e382a31aeae8
Seth Ayush Vijay <45192995+AyushSeth@users.noreply.github.com>
2021-02-17 19:38:15 +0530

    Add files via upload

---
+++ Trapping Rain Water/Trapping Rain Water.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+
+int trappingWater(int a[], int n){
+    int ans = 0;
+    for(int i=1;i<n-1;i++) {
+        int left = a[i];
+        for(int j=0;j<i;j++) {
+            left = max(a[j],left);
+        }
+        int right = a[i];
+        for(int j=i+1;j<n;j++) {
+            right = max(a[j],right);
+        }
+        ans = ans + min(left,right) - a[i];
+    }
+    return ans;
+}
+
+// { Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        
+        int a[n];
+        
+        //adding elements to the array
+        for(int i =0;i<n;i++){
+            cin >> a[i];            
+        }
+        
+        //calling trappingWater() function
+        cout << trappingWater(a, n) << endl;
+        
+    }
+    
+    return 0;
+}  // } Driver Code Ends
